386c21627d046472ce29a60f605b5a4d7a670dcd
Nikita Smolyaninov <nikita.smolaninov@gmail.com>
2021-03-09 18:22:12 +0200

    Basic script
    Script has only one main functon - "heapSort"
    Absolutely non-tested

---
+++ HeapSort.js
@@ -0,0 +1,47 @@
+function heapify(arr, length, i)
+{
+	let largest = i
+	let left = i * 2 + 1
+	let right = left + 1
+
+	if(arr[left] > arr[largest])
+	{
+		largest = left
+	}
+
+	if(arr[right] > arr[largest])
+	{
+		largest = right
+	}
+
+	if(largest != i)
+	{
+		[arr[i], arr[largest]] = [arr[largest], arr[i]]
+
+		heapify(arr, length, largest)
+	}
+
+	return arr
+}
+
+function heapSort(arr)
+{
+	let length = arr.length
+	let i = Math.floor(length / 2 - 1)
+	let k = length - 1
+
+	while(i >= 0)
+	{
+		heapify(arr, length, i)
+		i--
+	}
+
+	while(k >= 0)
+	{
+		[arr[0], arr[k]] = [arr[k]]
+		heapify(arr, k, 0)
+		k--
+	}
+
+	return arr
+}
